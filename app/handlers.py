51ab1f255b4b9caa50f567dbac94b35da98131f9
x0deex <x0ddexx@gmail.com>
2026-09-15 19:01:01 +0300

    DATABASE

--- app/handlers.py
+++ app/handlers.py
@@ -3,9 +3,10 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, CommandObject, Command
 
 import app.keyboards as kb
-
+from app.database.requests import set_user
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    await set_user(message.from_user.id)
     await message.answer("Добро пожаловать!", reply_markup=kb.reply_menu)
